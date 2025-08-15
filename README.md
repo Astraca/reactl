c23c0d498634d6de8b7e1d121aabf4243762b2d6
冯灏 <fenghao493@gmail.com>
2025-08-15 11:56:54 +0800

    useImmer

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
 # reactl
 learn log for react
+
+## useReducer
+
+useReducer(reducer,  initVale, initFunc)
+
+没有`initFunc`的话，默认值就是`initValue`，否则初始值就是经过`initFunc`处理完成的函数。
+
+```tsx
+import { useReducer } from "react";
+
+interface State {
+    count: number;
+    name: string;
+}
+
+interface Action {
+    type: string;
+}
+
+export default function App() {
+    const reducer = (state: State, action: Action) => {
+        switch (action.type) {
+            case "add":
+                return {...state, count: state.count + 1}
+            case "sub":
+                return {...state, count: state.count - 1}
+            default:
+                return state;	
+        }
+    }
+    
+    const [state, dispatch] = useReducer(reducer, {count: 0, name: "Astraca"});
+    
+    return(
+        <div>
+        	<h3>当前数字：{state.count}</h3>
+            <h3>当前名字：{state.name}</h3>
+            <button onClick={() => dispatch({type: "add"})}>+ 1</button>
+            <button onClick={() => dispatch({type: "sub"})}>- 1</button>
+        </div>
+    )
+}
+```
+
+ 
+
+
+
+## useImmer
+
+是社区维护的。
+
+1. 安装：`npm install immer use-immer`。
+2. 引用：`import { useImmer } from "use-immer"`
+3. 使用:
+    > 1. 用在对象和数组这种复杂类型，可以直接修改数据，可以使用内置API，比如数组的push、pop等。
+    > 1. 用在简单数据类型，和`useState`使用方式一样。
+
+### useImmerReducer
+
+1. 引用：`import { useImmerReducer } from "use-immer";`
+
+2. ~~~tsx
+   import { useReducer } from "react";
+   import { useImmerReducer } from "use-immer";
+   
+   interface State {
+       count: number;
+       name: string;
+   }
+   
+   interface Action {
+       type: string;
+   }
+   
+   export default function App() {
+       const reducer = (state: State, action: Action) => {
+           // 修改switch, 不需要返回state。
+           // 需要加 break
+           switch (action.type) {
+               case "add":
+                   state.count += 1
+                   break
+               case "sub":
+                   state.count -= 1
+                   break
+               default:
+                   return state;	
+           }
+       }
+       
+       const [state, dispatch] = useReducer(reducer, {count: 0, name: "Astraca"});
+       
+       return(
+           <div>
+           	<h3>当前数字：{state.count}</h3>
+               <h3>当前名字：{state.name}</h3>
+               <button onClick={() => dispatch({type: "add"})}>+ 1</button>
+               <button onClick={() => dispatch({type: "sub"})}>- 1</button>
+           </div>
+       )
+   }
+   ~~~
+
+3. 
--- src/App.tsx
+++ src/App.tsx
@@ -1,126 +1,34 @@
-import { useState, useReducer } from "react"
-import { useImmer } from "use-immer"
+import { useReducer } from "react";
 
-const initData = [
-    { name: '小满(只)', price: 100, count: 1, id: 1, isEdit: false },
-    { name: '中满(只)', price: 200, count: 1, id: 2, isEdit: false },
-    { name: '大满(只)', price: 300, count: 1, id: 3, isEdit: false }
-]
-type Data = typeof initData
+interface State {
+    count: number;
+    name: string;
+}
 
-const reducer = (state: Data, action: {type: 'add' | 'sub' | 'delete' | 'edit' | 'update_name' | 'blur', id: number, newName?: string}) => {
-    const item = state.find(item => item.id === action.id)!
-    switch (action.type) {
-        case 'add':
-            item.count++
-            return [...state]
-        case 'sub':
-            item.count--
-            if(item.count < 0) item.count = 0
-            return [...state]
-        case 'delete':
-            return state.filter((item) => item.id != action.id)
-        case 'edit':
-            item.isEdit = !item.isEdit
-            return [...state]
-        case 'update_name':
-            item.name = action.newName!
-            return [...state]
-        case 'blur':
-            item.isEdit = false
-            return [...state]
-        default:
-            return state
+interface Action {
+    type: string;
+}
+
+export default function App() {
+    const reducer = (state: State, action: Action) => {
+        switch (action.type) {
+            case "add":
+                return {...state, count: state.count + 1}
+            case "sub":
+                return {...state, count: state.count - 1}
+            default:
+                return state;	
+        }
     }
     
-    return state
-}
-function App() {
-
-    const [data, dispatch] = useReducer(reducer, initData)
-
-    const [person, setPerson] = useImmer([
-        {
-            name: "fenghao",
-            age: 21
-        },
-        {
-            name: "fenghao1",
-            age: 212
-        },
-        {
-            name: "fenghao2",
-            age: 213
-        }
-    ])
+    const [state, dispatch] = useReducer(reducer, {count: 0, name: "Astraca"});
     
     return(
-        <>
-            <h1>购物车</h1>
-            <table border={1} width={800} cellPadding={0} cellSpacing={0}>
-                <thead>
-                    <tr>
-                        <th>商品</th>
-                        <th>单价</th>
-                        <th>数量</th>
-                        <th>总价</th>
-                        <th>操作</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        data.map((item) => {
-                            return <tr key={item.id}>
-                                <td align="center">
-                                    {item.isEdit ? <input type="text" value={item.name} onBlur={() => dispatch({type: 'blur', id: item.id})} onChange={(e) => dispatch({type: 'update_name', id:item.id, newName: e.target.value})}/> : item.name}
-                                </td>
-                                <td align="center">{item.price}</td>
-                                <td align="center">
-                                    <button onClick={() => dispatch({type: 'add', id: item.id})}>+</button>
-                                    {item.count}
-                                    <button onClick={() => dispatch({type: 'sub', id: item.id})}>-</button>
-                                </td>
-                                <td align="center">{item.price * item.count}</td>
-                                <td align="center">
-                                    <button onClick={() => dispatch({type: 'edit', id: item.id})}>修改</button>
-                                    <button onClick={() => dispatch({type: 'delete', id: item.id})}>删除</button>
-                                </td>
-                            </tr>
-                        })
-                    }
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colSpan={4}></td>
-                        <td align="right">总价：{data.reduce((a, b) => a + b.price * b.count, 0)}</td>
-                    </tr>
-                </tfoot>
-            </table>
-
-            <hr />
-
-            <div>
-                {
-                    person.map(item => {
-                        return (
-                            <>
-                                <span>{item.name}</span> - <span>{item.age}</span> <br />
-                            </>
-                        )
-                        
-                    })
-                }
-
-                <button onClick={() => {
-                    setPerson(draft => {
-                        // draft.hobby.name = "游泳"
-                    })
-                }}>
-                    修改玩
-                </button>
-            </div>
-        </>
+        <div>
+        	<h3>当前数字：{state.count}</h3>
+            <h3>当前名字：{state.name}</h3>
+            <button onClick={() => dispatch({type: "add"})}>+ 1</button>
+            <button onClick={() => dispatch({type: "sub"})}>- 1</button>
+        </div>
     )
 }
-
-export default App

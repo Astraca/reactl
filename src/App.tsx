3036a77848411c8e9b6c063df7f1e74f8cece88c
冯灏 <fenghao493@gmail.com>
2025-08-15 13:43:45 +0800

    useSyncExternalStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,15 @@
-import { useImmerReducer } from "use-immer";
+import { useStorage } from "./hooks/useStorage.ts"
 
-interface State {
-    count: number;
-    name: string;
-}
-
-interface Action {
-    type: string;
-}
-
-export default function App() {
-    const reducer = (state: State, action: Action) => {
-        // 修改switch, 不需要返回state。
-        // 需要加 break
-        switch (action.type) {
-            case "add":
-                state.count += 1
-                break
-            case "sub":
-                state.count -= 1
-                break
-            case "update_name":
-                state.name = "晴川"
-                break
-            default:
-                return state;	
-        }
-    }
+function App() {
+    const [count, setCount] = useStorage('count', 1)
     
-    const [state, dispatch] = useImmerReducer(reducer, {count: 0, name: "Astraca"});
-    
-    return(
-        <div>
-        	<h3>当前数字：{state.count}</h3>
-            <h3>当前名字：{state.name}</h3>
-            <button onClick={() => dispatch({type: "add"})}>+ 1</button>
-            <button onClick={() => dispatch({type: "sub"})}>- 1</button>
-            <button onClick={() => dispatch({type: "update_name"})}>Update_name</button>
-        </div>
+    return (
+        <>
+            <h1>Value: {count}</h1>
+            <button onClick={() => setCount(count + 1)}>+1</button>
+            <button onClick={() => setCount(count - 1)}>-1</button>
+        </>
     )
 }
+
+export default App
---
+++ src/hooks/useStorage.ts
@@ -0,0 +1,29 @@
+import { useSyncExternalStore } from "react"
+
+export const useStorage = (key:string, initialValue:any) => {
+
+    // 订阅者
+    const subscribe = (callback: () => void) => {
+        // 订阅浏览器API
+        window.addEventListener('storage', callback)  // 浏览器storage发生变化就执行callback
+        // 需要返回一个函数
+        return () => {
+            // 返回取消订阅
+        }
+    }
+
+    // 返回当前的快照
+    const getSnapshot = () => {
+        return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)!) : initialValue
+    }
+
+    const res = useSyncExternalStore(subscribe, getSnapshot)
+
+    const updateStorage = (value:any) => {
+        localStorage.setItem(key, JSON.stringify(value))
+        // 手动触发Storage事件
+        window.dispatchEvent(new StorageEvent('storage'))
+    }
+
+    return [res, updateStorage]
+}
